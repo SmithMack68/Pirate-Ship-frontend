bf7706540ff289c45ecebc7e442a8dd49e3d9b96
Candice Smith MacKenzie <Mahony8@aol.com>
2022-05-29 21:38:31 -0400

    added dependencies

---
+++ NOTES.md
@@ -0,0 +1,33 @@
+### Name of App
+A user can:
+1. Create a ?
+2. Get a list of ? with ?(ex: ingredients)
+3. Delete ?
+4. View ? (ex: recipes)
+5. Randomly select ?
+6. Edit a ? (stretch goal)
+
+
+### Features
+* Homepage
+* NavBar
+* List page
+* View Details page
+* Create Form page
+* Delete ?
+* Edit page
+* Randomly select ?
+
+
+### MVP
+* Homepage
+* NavBar
+* List page
+* View Details page
+* Create Form page
+* Delete ?
+
+### Stretch Goals (quality of life updates make it more user friendly)
+* Edit page
+* Randomly select ?
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, makeStyles } from '@material-ui/core';
 
-function App() {
+
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: 'blue',
+    padding: "10px"
+  }
+})
+
+const App = () => {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Button variant="contained" className= { classes.button }>Click Me</Button>
     </div>
   );
 }
